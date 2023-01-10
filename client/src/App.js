32d0caf64fa3eb65e9058e36813cae5e67559886
SO-JIMM <jamesdgrace@gmail.com>
2023-01-10 22:58:17 +1100

    remove proxy, add gh-pages, fetch localhost

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React from "react"
 
 const App = () => {
+  const [data, setData] = React.useState(null)
+  React.useEffect(() => {
+    fetch("http://localhost:3001")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.message)
+      })
+  }, [])
   return <div className='App'></div>
 }
 
