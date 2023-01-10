feae5e6cff7ec4f9c5848bf0ec3bc56b13a76f35
SO-JIMM <jamesdgrace@gmail.com>
2023-01-10 22:58:56 +1100

    fix

--- server/index.js
+++ server/index.js
@@ -1,20 +1,31 @@
 const express = require("express")
-const app = express()
 const cors = require("cors")
 
+const app = express()
+
 app.use(cors())
-app.use(express.json())
 
-app.use(express.static(path.join(__dirname, "build")))
+// app.use(express.static(path.join(__dirname, "public")))
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")))
+// // Serve the static files from the React app
+// app.use(express.static(path.join(__dirname, "client/build")))
 
 // Handles any requests that don't match the ones above
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"))
+// app.get("*", (req, res) => {
+// res.sendFile(path.join(__dirname + "/client/build/index.html"))
+// })
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+}
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express!" })
 })
 
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"))
+// })
 const port = process.env.PORT || 3001
 app.listen(port)
 
