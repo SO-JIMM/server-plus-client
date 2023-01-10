ac418498d8d9ff2dcf22f56e6c1d4c3e6d2d2995
SO-JIMM <jamesdgrace@gmail.com>
2023-01-10 23:03:19 +1100

    allow for production port

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import React from "react"
 const App = () => {
   const [data, setData] = React.useState(null)
   React.useEffect(() => {
-    fetch("http://localhost:3001")
+    fetch(`${process.env.PORT || "http://localhost:3001"}`)
       .then((res) => res.json())
       .then((data) => {
         setData(data.message)
+        console.log(data.message)
       })
+      .catch((e) => console.log(e))
   }, [])
   return <div className='App'></div>
 }
